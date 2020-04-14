de04c54c03eb900bae9b612b0fcfc1610cdb4746
tyfts1 <romannawaz01@gmail.com>
2020-04-14 20:27:39 +0300

    about/login/navbar

--- client/DeliveryHome/src/app/app.module.ts
+++ client/DeliveryHome/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
